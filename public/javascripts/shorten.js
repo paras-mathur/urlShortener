d106633610ba46817e22b17d0dd7f72c0cca1ff6
paras mathur <paras.mathur@247-inc.com>
2017-06-06 15:35:43 +0530

    Repo Push

---
+++ public/javascripts/shorten.js
@@ -0,0 +1,18 @@
+$('.btn-shorten').on('click', function(){
+
+  $.ajax({
+    url: '/api/shorten',
+    type: 'POST',
+    dataType: 'JSON',
+    data: {url: $('#url-field').val()},
+    success: function(data){
+        var resultHTML = '<a class="result" href="' + data.shortUrl + '">'
+            + data.shortUrl + '</a>';
+        //$('#link').html(resultHTML);
+        //$('#shortUrl').html(resultHTML);
+        new $.Zebra_Dialog(resultHTML);  
+       
+    }
+  });
+
+});
